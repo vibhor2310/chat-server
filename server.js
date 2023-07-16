15910d482860696af2d3f210d785b20974fe35b9
vibhor2310 <vibhorsharma23oct@gmail.com>
2023-07-16 11:37:05 +0530

    changes

--- server.js
+++ server.js
@@ -12,16 +12,18 @@ dotenv.config();
 connectDB();
 const app = express();
 
-app.use(express.json()); // to accept json data
-
-// app.get("/", (req, res) => {
-//   res.send("API Running!");
-// });
 app.use(cors(
   {
    origin :['https://chat-frontend-0kgj.onrender.com/']
 }
 ));
+
+app.use(express.json()); // to accept json data
+
+// app.get("/", (req, res) => {
+//   res.send("API Running!");
+// });
+
 app.use("/api/user", userRoutes);
 app.use("/api/chat", chatRoutes);
 app.use("/api/message", messageRoutes);
